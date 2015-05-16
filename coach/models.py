d6f7a74ccfe1dc3280222555846c6214788bc4de
oferel <oferel1@gmail>
2015-05-16 23:43:53 +0300

    model

--- coach/models.py
+++ coach/models.py
@@ -1,5 +1,14 @@
-'''
-Created on 22 9 2015
+from django.db import models
 
-@author: OferEl
-'''
+class coach(models.Model):
+    coach_id = models.IntegerField(max_length=30)
+    coach_name = models.CharField(max_length=50)
+    coach_lastname = models.CharField(max_length=60)
+    coach_area = models.CharField(max_length=30)
+    coach_city = models.CharField(max_length=50)
+    coach_status = models.CharField(max_length=1)
+    create_date = models.DateField()
+    
+    def __unicode__(self):
+        return self.name
+
--- users/models.py
+++ users/models.py
@@ -1,5 +1,17 @@
-'''
-Created on 22 9 2015
+from django.db import models
 
-@author: OferEl
-'''
+
+class user(models.Model):
+    user_id = models.IntegerField(max_length=30)
+    user_name = models.CharField(max_length=50)
+    user_lastname = models.CharField(max_length=60)
+    user_city = models.CharField(max_length=50)
+    user_status = models.CharField(max_length=1)
+    user_email = models.EmailField()
+    gender = models.CharField(max_length=1)
+    password = models.CharField(max_length=50)
+    password_date = models.DateField()
+    create_date = models.DateField()
+    
+    def __unicode__(self):
+        return self.name
